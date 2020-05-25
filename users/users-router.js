c82e6692e6ae5d44625b25d3624dbe663e890632
david <davidealvarez1@gmail.com>
2020-05-25 14:18:05 -0700

    registered first user couldnt successfully seed

--- users/users-router.js
+++ users/users-router.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const Users = require('./users-model.js');
-const router = express.Router();
-const {isValid} = require('./isValid.js');
+const express = require('express');//server
+const Users = require('./users-model.js');//model
+const router = express.Router();//for routes
+const {isValid} = require('./isValid.js');//checks to see if "user" has username & pass
+const bcrypt = require('bcryptjs');//hashes password
 
-router.post('/register', (req,res) => {
-    
-    Users.add
+router.post('/register', (req,res) => {//add user w/hashed password to db
+    const user = req.body;
+    if(isValid(user)){
+        const hash = bcrypt.hashSync(user.password, 8);
+        user.password = hash;
+        Users.add(user)
+            .then(user => {
+                console.log("user: ", user)
+                res.status(201).send(user)
+            })
+            .catch(error => {
+                res.status(500).json(error)
+            })
+    }else{
+        res.status(400).json({ message: "couldnt register user; add username and password" });
+    }
 })
 
 module.exports = router;
